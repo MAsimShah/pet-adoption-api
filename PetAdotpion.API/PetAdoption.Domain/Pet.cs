8710753b6e0194059838ebba4bead57b18f41256
MAsim <shahasim190@gmail.com>
2025-05-22 16:21:44 +0500

    create web api using clean architecture

---
+++ PetAdotpion.API/PetAdoption.Domain/Pet.cs
@@ -0,0 +1,21 @@
+﻿using PetAdoption.Domain;
+
+namespace PetAdoption.Domain
+{
+    public class Pet : BaseEntity
+    {
+        public string Name { get; set; } = string.Empty;
+        public string Type { get; set; } = string.Empty;
+        public string Breed { get; set; } = string.Empty;
+        public int Age { get; set; }
+        public string Gender { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public string ImageUrl { get; set; } = string.Empty;
+        public bool IsAdopted { get; set; } = false;
+
+        public string? ImageBase64 { get; set; } // Store image as Base64
+        public string UserId { get; set; }
+
+        //public virtual ApplicationUser User { get; set; }
+    }
+}
