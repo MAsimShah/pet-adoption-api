2b9a2b18a4072654b56554d0fa7615f7c740315c
MAsim <shahasim190@gmail.com>
2025-07-07 20:44:37 +0500

    auth validation

--- PetAdotpion.API/PetAdoption.Application/Interfaces/IAuthService.cs
+++ PetAdotpion.API/PetAdoption.Application/Interfaces/IAuthService.cs
@@ -7,10 +7,13 @@ namespace PetAdoption.Application.Interfaces
     public interface IAuthService
     {
         Task<User> GetUser(Expression<Func<User, bool>> predicate);
+
         Task<User> RegisterUserAsync(RegisterDTO model);
 
         Task<TokenResponseDTO> LoginUserAsync(User user);
 
         Task<bool> CheckUsePassword(User user, string password);
+
+        Task<TokenResponseDTO> RegenrateToken(User user);
     }
 }
