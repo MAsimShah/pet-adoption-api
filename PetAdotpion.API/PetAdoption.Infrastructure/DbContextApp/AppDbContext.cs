2871a8be2b5cfc21ec7a23a118cbe27b34c30992
MAsim <shahasim190@gmail.com>
2025-07-09 22:39:01 +0500

    remove all migrations

--- PetAdotpion.API/PetAdoption.Infrastructure/DbContextApp/AppDbContext.cs
+++ PetAdotpion.API/PetAdoption.Infrastructure/DbContextApp/AppDbContext.cs
@@ -16,6 +16,27 @@ namespace PetAdoption.Infrastructure.DbContextApp
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
+
+            // Hardcoded password hash for password "admin"
+            const string passwordHash = "AQAAAAEAACcQAAAAEGyPWqF74/ivwz6iKdN9UXeb1rSknH1XR+cvV3YfTnRDt8CvwP6UOwASgXcF3w==";
+
+            // Seeding admin user
+            modelBuilder.Entity<User>().HasData(
+                new User
+                {
+                    Id = "a1111111-aaaa-aaaa-aaaa-aaaaaaaaaaaa", // Static GUID as string
+                    UserName = "admin",
+                    NormalizedUserName = "ADMIN",
+                    Email = "admin@admin.com",
+                    NormalizedEmail = "ADMIN@ADMIN.COM",
+                    EmailConfirmed = true,
+                    PasswordHash = passwordHash,
+                    SecurityStamp = "STATIC_SECURITY_STAMP", // Must be static too
+                    ConcurrencyStamp = "STATIC_CONCURRENCY_STAMP" // Must be static
+                }
+            );
+
+
         }
     }
 }
